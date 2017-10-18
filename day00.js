98a202e88d4a1a495c454019b503508fe34c83b4
faithfem <fmugure@intracitygeeks.org>
2017-10-17 20:55:09 -0400

    update

---
+++ day00.js
@@ -0,0 +1,6 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log("HackerRank is the best!");
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
--- day01.js
+++ day01.js
@@ -1,6 +1,21 @@
-function processData(inputString) {
-    // This line of code prints the first line of output
-    console.log("Hello, World.");
-    console.log("HackerRank is the best!");
-    // Write the second line of output that prints the contents of 'inputString' here.
-}
+/* Declare second integer, double, and String variables. */
+int ii;
+double dd;
+String ss;
+/* Read and save an integer, double, and String to your variables.*/
+ii = 12;
+dd = 4.0;
+ss = "is the best place to learn and practice coding!";
+
+// Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+System.out.println(i + ii);
+/* Print the sum of both integer variables on a new line. */
+
+System.out.println(d + dd);
+/* Print the sum of the double variables on a new line. */
+
+System.out.println(s + ss);
+/* Concatenate and print the String variables on a new line;
+s+ss;
+  the 's' variable above should be printed first. */
